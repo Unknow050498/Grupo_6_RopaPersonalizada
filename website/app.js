c79546ab7d4549ddebcdb8fcf2716a8be81b74ee
Unknown050498 <jo_carlos07@hotmail.com>
2021-10-28 13:08:18 -0500

    Actualización 2

---
+++ website/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo')
+})
+
+app.listen(3000, () => {
+    console.log('Corriendo servidor.');
+});
