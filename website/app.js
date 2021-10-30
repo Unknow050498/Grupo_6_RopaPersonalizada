3446166b047e286e68b164bc29df992da53bb999
Unknown050498 <jo_carlos07@hotmail.com>
2021-10-30 17:24:57 -0500

    Index.html con header, Style.css estilos del header, carpeta Public con carpetas CSS e IMG

--- website/app.js
+++ website/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo')
-})
+
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html/'));
+});
+
+app.use('/', express.static('public'));
 
 app.listen(3000, () => {
     console.log('Corriendo servidor.');
-});
+}); 
