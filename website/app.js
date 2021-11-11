9f7eb21fcfeb1d68c48559ccf1e6e95ad3f4998c
Unknown050498 <jo_carlos07@hotmail.com>
2021-11-11 00:17:28 -0600

    Exrepess corriendo

--- website/app.js
+++ website/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
 app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html/'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.use('/', express.static('public'));
