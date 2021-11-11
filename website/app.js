5345a931be2396ade570ca30d36f126a606e83f3
Unknown050498 <jo_carlos07@hotmail.com>
2021-11-11 00:20:55 -0600

    Servidor express

--- website/app.js
+++ website/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+/*==================== LLAMA A LOS ARCHIVOS DEL VIEWS ====================*/
 app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+/*==================== CONECTA CON LA CARPETA PUBLIC ====================*/
 app.use('/', express.static('public'));
 
+/*==================== CABRE EL PUERTO 3000 ====================*/
 app.listen(3000, () => {
     console.log('Corriendo servidor.');
 }); 
