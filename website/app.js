063cfa7c19ebdb62628d3991fc835c66b92f95ec
SandyDSM <sad.zoe@gmail.com>
2021-11-20 11:28:02 -0600

    Actualizo archivos, index, productos, css y app

--- website/app.js
+++ website/app.js
@@ -11,6 +11,14 @@ app.get('/producto',(req, res) => {
     res.sendFile(path.join(__dirname, 'views/producto.html'));
 });
 
+app.get('/login',(req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register',(req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
 /*==================== CONECTA CON LA CARPETA PUBLIC ====================*/
 app.use('/', express.static('public'));
 
