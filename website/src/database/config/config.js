fba444c20e24ee196b2d6d069dc576b263375ce4
Unknown050498 <jo_carlos07@hotmail.com>
2022-02-24 21:05:44 -0600

    Correxión de tamaños en products

--- website/src/database/config/config.js
+++ website/src/database/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'delossantos',
+    "password": "",
     "database": "coolcloth1_db",
     "host": "127.0.0.1",
+    "port": "3307",
     "dialect": "mysql"
   },
   "test": {
