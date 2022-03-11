f0ad403e549fc9fbf26989dfe26fa21cc5ec61a8
SandyDSM <sad.zoe@gmail.com>
2022-03-11 15:43:55 -0600

    validación back

--- website/src/controllers/userController.js
+++ website/src/controllers/userController.js
@@ -3,7 +3,8 @@ const path = require("path");
 const { fileURLToPath } = require("url");
 const db = require("../database/models");
 const sequelize = db.sequelize;
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 //const usersFilePath = path.join(__dirname, '../data/UsersDataBase.json');
 //let usersJSON = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -14,7 +15,7 @@ const usersC = {
   list: (req, res) => {
     User.findAll()
       .then(function (usuarios) {
-        res.render("users.ejs", {usuarios});
+        res.render("users.ejs", { usuarios });
       })
       .catch((error) => {
         res.send(error);
@@ -25,28 +26,35 @@ const usersC = {
   },
   createUser: function (req, res) {
     let passhash = bcryptjs.hashSync(req.body.passwordSingUp, 10);
-    if (req.file) {
-      User.create({
-        user_employ: req.body.usernameSignUp,
-        name_e: req.body.nameSignUp,
-        email_e: req.body.emailSignUp,
-        password_e: passhash,
-        dateborn_e: req.body.nacimiento,
-        sex_e:req.body.radioSex,
-        type_employ: req.body.typeSignUp,
-        imgProfile: req.file.filename,
-      })
-        .then(function () {
-          return res.redirect("/users");
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
+      if (req.file) {
+        User.create({
+          user_employ: req.body.usernameSignUp,
+          name_e: req.body.nameSignUp,
+          email_e: req.body.emailSignUp,
+          password_e: passhash,
+          dateborn_e: req.body.nacimiento,
+          sex_e: req.body.radioSex,
+          type_employ: req.body.typeSignUp,
+          imgProfile: req.file.filename,
         })
-        .catch((error) => res.send(error));
-    } 
+          .then(function () {
+            return res.redirect("/users");
+          })
+          .catch((e) => res.send(e));
+      }
+    }else{
+     res.render("signupAdmin.ejs", {errors:errors.array()});
+     console.log(errors.array());
+     //res.send(errors);
+    }
   },
   detailsU: (req, res) => {
     const userName = req.params.userName;
     User.findByPk(userName)
       .then(function (usuario) {
-        res.render("userDetail", {usuario});
+        res.render("userDetail", { usuario });
       })
       .catch((error) => res.send(error));
   },
@@ -54,39 +62,43 @@ const usersC = {
     const userName = req.params.userName;
     User.findByPk(userName)
       .then(function (usuario) {
-        res.render("userEdit", {usuario});
+        res.render("userEdit", { usuario });
       })
       .catch((error) => res.send(error));
   },
   updateU: function (req, res) {
     const userName = req.params.userName;
-      User.update({
+    User.update(
+      {
         user_employ: req.body.usernameSignUp,
         name_e: req.body.nameSignUp,
         email_e: req.body.emailSignUp,
         password_e: req.body.passwordSingUp,
         dateborn_e: req.body.nacimiento,
-        sex_e:req.body.radioSex,
+        sex_e: req.body.radioSex,
         type_employ: req.body.typeSignUp,
         imgProfile: req.file.filename,
       },
       {
-          where: {user_employ: userName}
+        where: { user_employ: userName },
+      }
+    )
+      .then(function () {
+        return res.redirect("/users");
       })
-        .then(function () {
-          return res.redirect("/users");
-        })
-        .catch((error) => res.send(error));
-    
+      .catch((error) => res.send(error));
   },
-  destroy: function (req,res) {
+  destroy: function (req, res) {
     const userName = req.params.userName;
     User.destroy({
-        where: {user_employ: userName}, force: true})
-    .then(()=>{
-        return res.redirect('/users')})
-    .catch(error => res.send(error)) 
-}
+      where: { user_employ: userName },
+      force: true,
+    })
+      .then(() => {
+        return res.redirect("/users");
+      })
+      .catch((error) => res.send(error));
+  },
 };
 
 module.exports = usersC;
--- website/src/routes/userRoutes.js
+++ website/src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const app = require('../app');
 const permisionMiddle = require('../middleware/permisionMiddle');
+const {body} = require('express-validator');
 
 
 const usersC = require('../controllers/userController');
@@ -20,11 +21,21 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage});
 
+//Validaciones
+const validaCreateForm =[
+    body('nameSignUp').notEmpty().withMessage('El campo de nombre no puede estar vacio'),
+    body('usernameSignUp').notEmpty().withMessage('El campo de nombre no puede estar vacio'),
+    body('emailSignUp').isEmail().withMessage('El email debe ser un formato valido'),
+    body('password').isEmpty().withMessage('El campo de contraseña no puede estar vacio'),
+    body('nacimiento').isEmpty().withMessage('Porfavor coloca un fecha'),
+    body('radioSex').isEmpty().withMessage('Elige un genero'),
+    body('typeSignUp').isEmpty().withMessage('Asigna un cargo'),
+];
 
 usersR.get('/users', usersC.list);
 usersR.get('/users/detailsU/:userName', usersC.detailsU);
-usersR.get('/users/addUser', permisionMiddle, usersC.addUser);
-usersR.post('/users/addUser', upload.single('uploadedImageUser'), usersC.createUser);
+usersR.get('/users/addUser', usersC.addUser);
+usersR.post('/users/addUser', upload.single('uploadedImageUser'), validaCreateForm, usersC.createUser);
 usersR.get('/users/edit/:userName', permisionMiddle, usersC.editU);
 usersR.put('/users/edit/:userName', upload.single('uploadedImageUserEdit'), usersC.updateU);
 usersR.delete('/users/delete/:userName', usersC.destroy);
